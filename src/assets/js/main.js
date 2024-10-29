5acf6d695319add4a7d00c2bfdc4511d3ce69875
Bakhriev <muradbakhriev@gmail.com>
2024-10-29 11:16:33 +0500

    last

---
+++ src/assets/js/main.js
@@ -0,0 +1,20 @@
+const filter = () => {
+	const body = document.body;
+	const filterForm = document.querySelector('.filters');
+	const filterTrigger = document.querySelector('[data-trigger="filter"]');
+	const closeBtn = filterForm.querySelector('[data-destroyer]');
+
+	if (!filterForm && !filterTrigger) return;
+
+	filterTrigger.addEventListener('click', () => {
+		filterForm.classList.toggle('active');
+		body.classList.toggle('scroll-lock');
+	});
+
+	closeBtn.addEventListener('click', () => {
+		filterForm.classList.remove('active');
+		body.classList.remove('scroll-lock');
+	});
+};
+
+filter();
