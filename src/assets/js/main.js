856fbe2ada7f54611eb828a534cb8480607d45d3
Bakhriev <muradbakhriev@gmail.com>
2024-10-29 11:24:57 +0500

    add active class on click filter btn

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,11 +9,13 @@ const filter = () => {
 	filterTrigger.addEventListener('click', () => {
 		filterForm.classList.toggle('active');
 		body.classList.toggle('scroll-lock');
+		filterTrigger.classList.toggle('active');
 	});
 
 	closeBtn.addEventListener('click', () => {
 		filterForm.classList.remove('active');
 		body.classList.remove('scroll-lock');
+		filterTrigger.classList.remove('active');
 	});
 };
 
